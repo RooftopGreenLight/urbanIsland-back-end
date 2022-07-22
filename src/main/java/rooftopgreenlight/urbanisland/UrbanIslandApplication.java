ee46c203ca1d2f5554228835cfd0d55ae3ccb8db
최영찬 <gks54410@gmail.com>
2022-07-23 00:19:34 +0900

    Initial Commit

---
+++ src/main/java/rooftopgreenlight/urbanisland/UrbanIslandApplication.java
@@ -0,0 +1,13 @@
+package rooftopgreenlight.urbanisland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanIslandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanIslandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rooftopgreenlight/urbanisland/UrbanIslandApplicationTests.java
@@ -0,0 +1,13 @@
+package rooftopgreenlight.urbanisland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanIslandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
