4f63151d882e68c797b75784d2204019dfa18595
최영찬 <gks54410@gmail.com>
2022-07-26 01:39:48 +0900

    Feat: #7 - Swagger 기능 구현
    Swagger Config 구현 및 Controller 기능 문서화

--- src/main/java/rooftopgreenlight/urbanisland/api/controller/AuthController.java
+++ src/main/java/rooftopgreenlight/urbanisland/api/controller/AuthController.java
@@ -1,5 +1,8 @@
 package rooftopgreenlight.urbanisland.api.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,18 +28,25 @@ public class AuthController {
 
     @GetMapping("/health")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "서버 정상 동작하는지 확인", notes = "정상 동작 시 '서버 동작 중' 반환")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "API 정상 동작"),
+            @ApiResponse(code = 500, message = "서버 문제")
+    })
     public ResponseDto health() {
         return new ResponseDto("서버 동작 중...");
     }
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "로그인 기능", notes = "정상 동작 시 로그인 성공")
     public ResponseDto login(@Validated @RequestBody RequestLoginDto loginDto) {
         return new ResponseDto(authService.login(loginDto.getEmail(), loginDto.getPassword()));
     }
 
     @PostMapping("/join")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "회원가입 기능", notes = "정상 동작 시 회원가입 성공" )
     public ResponseDto join(@Validated @RequestBody RequestJoinDto joinDto) {
         memberService.save(createMember(joinDto));
         
