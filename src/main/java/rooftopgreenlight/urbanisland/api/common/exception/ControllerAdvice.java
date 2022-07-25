9eb35217de763da65673d95f24535a0c9baadcf4
최영찬 <gks54410@gmail.com>
2022-07-26 00:56:37 +0900

    Feat: #1 - 예외 처리 추가
    Member 조회 예외 처리

--- src/main/java/rooftopgreenlight/urbanisland/api/common/exception/ControllerAdvice.java
+++ src/main/java/rooftopgreenlight/urbanisland/api/common/exception/ControllerAdvice.java
@@ -9,20 +9,34 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import rooftopgreenlight.urbanisland.api.common.exception.dto.ErrorDetailDto;
 import rooftopgreenlight.urbanisland.api.common.exception.dto.ErrorDto;
 import rooftopgreenlight.urbanisland.api.controller.dto.ResponseDto;
+import rooftopgreenlight.urbanisland.domain.exception.NotFoundMemberException;
 
 @Slf4j
 @RestControllerAdvice
 public class ControllerAdvice {
 
+    /**
+     * 요청 데이터 검증 오류 시
+     * @param e
+     * @return 에러 정보 전달
+     */
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseDto validationError(MethodArgumentNotValidException e){
+    public ResponseDto validationError(MethodArgumentNotValidException e) {
         ErrorDto errorDto = createError("회원 가입 실패!");
         e.getFieldErrors().forEach(
                 error -> errorDto.getErrors().add(new ErrorDetailDto(error.getField(), error.getDefaultMessage()))
         );
         return new ResponseDto(errorDto);
     }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseDto notFoundMemberException(NotFoundMemberException e) {
+        return new ResponseDto(createError(e.getMessage()));
+    }
+
+
 
     private ErrorDto createError(String errorMessage) {
         ErrorDto errorDto = new ErrorDto();
