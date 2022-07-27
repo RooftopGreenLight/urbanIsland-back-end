3e39779daa5e7cf209e92a9826a6d9065eaf0368
jiwon <ljwon77@daum.net>
2022-07-27 22:27:05 +0900

    Feat: #16 - Access-token 검증 추가

--- src/main/java/rooftopgreenlight/urbanisland/api/common/jwt/JwtFilter.java
+++ src/main/java/rooftopgreenlight/urbanisland/api/common/jwt/JwtFilter.java
@@ -1,10 +1,13 @@
 package rooftopgreenlight.urbanisland.api.common.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import rooftopgreenlight.urbanisland.api.controller.dto.ResponseDto;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -17,6 +20,7 @@ import java.io.IOException;
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtProvider jwtProvider;
+    private final ObjectMapper objectMapper;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -25,6 +29,12 @@ public class JwtFilter extends OncePerRequestFilter {
 
         if(refreshToken == null && authorization != null && authorization.startsWith("Bearer ")) {
             String token = authorization.replace("Bearer ", "");
+            if(!jwtProvider.isTokenValid(token)) {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                response.setContentType("application/json");
+                response.getWriter().write("Please give Refresh-Token.");
+                return;
+            }
             Authentication authentication = jwtProvider.getMemberInfo(token);
 
             SecurityContextHolder.getContext().setAuthentication(authentication);
