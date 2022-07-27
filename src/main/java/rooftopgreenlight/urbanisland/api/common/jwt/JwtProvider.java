e93de2f995f7460b2494d85531713c217faa9812
jiwon <ljwon77@daum.net>
2022-07-27 22:32:47 +0900

    Feat: #16 - token 유효성 검증 추가

--- src/main/java/rooftopgreenlight/urbanisland/api/common/jwt/JwtProvider.java
+++ src/main/java/rooftopgreenlight/urbanisland/api/common/jwt/JwtProvider.java
@@ -1,9 +1,6 @@
 package rooftopgreenlight.urbanisland.api.common.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtParser;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -81,4 +78,17 @@ public class JwtProvider {
 
         return new UsernamePasswordAuthenticationToken(principal, "", roleList);
     }
+
+    public boolean isTokenValid(String token) {
+        try {
+            JwtParser jwtParser = Jwts.parserBuilder().setSigningKey(key).build();
+            return !jwtParser.parseClaimsJws(token).getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            if(e instanceof ExpiredJwtException) {
+                return false;
+            }
+        }
+        return false;
+    }
+
 }
