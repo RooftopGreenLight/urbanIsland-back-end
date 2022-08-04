9598723d98278c35460a479870ef5cc8ba442f2c
최영찬 <gks54410@gmail.com>
2022-08-04 23:43:33 +0900

    Feat: #32 - 옥상 요청 Dto 추가

---
+++ src/main/java/rooftopgreenlight/urbanisland/api/controller/dto/RooftopRequest.java
@@ -0,0 +1,37 @@
+package rooftopgreenlight.urbanisland.api.controller.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class RooftopRequest {
+
+    @NotBlank(message = "올바른 넓이 입력 형식이 아닙니다.")
+    private String width;
+    private String explainContent;
+    private String refundContent;
+    private String roleContent;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime startTime;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime endTime;
+
+    private int adultCount;
+    private int kidCount;
+    private int petCount;
+    private int totalCount;
+
+    @NotBlank(message = "올바른 도/광역시 입력 형식이 아닙니다.")
+    private String county;
+    @NotBlank(message = "올바른 시/군/구 입력 형식이 아닙니다.")
+    private String city;
+    @NotBlank(message = "올바른 세부 주소 입력 형식이 아닙니다.")
+    private String detail;
+
+}
