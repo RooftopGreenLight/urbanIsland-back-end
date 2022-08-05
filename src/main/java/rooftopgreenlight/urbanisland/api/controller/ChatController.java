6fc20188d8c0b55f43f492ab6450a6a3f0b7219c
jiwon <ljwon77@daum.net>
2022-08-05 23:56:23 +0900

    Feat: Swagger ApiOperation 추가, 메시지 전송 시 반환 정보 수정

--- src/main/java/rooftopgreenlight/urbanisland/api/controller/ChatController.java
+++ src/main/java/rooftopgreenlight/urbanisland/api/controller/ChatController.java
@@ -1,9 +1,13 @@
 package rooftopgreenlight.urbanisland.api.controller;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import rooftopgreenlight.urbanisland.api.common.annotation.PK;
 import rooftopgreenlight.urbanisland.api.controller.dto.APIResponse;
 import rooftopgreenlight.urbanisland.api.controller.dto.ChatRequest;
@@ -13,33 +17,27 @@ import rooftopgreenlight.urbanisland.domain.chat.entity.Message;
 import rooftopgreenlight.urbanisland.domain.chat.service.ChatRoomService;
 import rooftopgreenlight.urbanisland.domain.chat.service.MessageService;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @MessageMapping : 발행 경로
- * @SendToUser : 1:1로 메시지로 보낼 때 사용, 경로는 /queue로 시작
- */
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/chat")
 public class ChatController {
-
     private final SimpMessagingTemplate messagingTemplate;
     private final ChatRoomService chatRoomService;
     private final MessageService messageService;
 
     /**
      * 문의 응답 확인하기
-     * = 사용자 입장에서 본 옥상지기-사용자 채팅방 목록
-     *
-     * 요청 데이터) 사용자 ID
-     * 응답 데이터) room_id에 따른 <가장 마지막으로 대답한> 사용자의 시간, 응답 내용 리스트
-     * 의문인 점
-     * - 기획서에서는 문의 응답 (옥상지기가 마지막으로 대답했을 때)과 문의 내용 (사용자가 마지막으로 대답했을 때)를 굳이 나눴는데
-     * 그냥 그럴 필요 없이 어떤 것이든 대답 온 거 누르면 채팅 내역이 뜨는 게 더 낫지 않나 싶다...
+     * @param memberId
+     * @return List<ChatRoomResponse>
      */
     @GetMapping("/inquiry/response")
+    @ApiOperation(value = "문의 응답 확인하기 (목록)",
+            notes = "정상 동작 시 채팅방 목록 전달, 내부 요소: roomId, memberId, content, sendTime")
     public APIResponse getInquiryResponse(@PK Long memberId) {
         List<ChatRoom> roomList = chatRoomService.getRoomList(memberId);
         List<ChatRoomResponse> roomResList = new ArrayList<>();
@@ -47,8 +45,7 @@ public class ChatController {
         roomList.stream().forEach(chatRoom -> {
             Message findMessage = messageService.getFirstMsgJoinFetchMember(memberId, chatRoom.getId());
             if(findMessage!=null) {
-                // 생각해봤는데 이러면 n+1 나오지 않나...? ㅎㅎ
-                roomResList.add(ChatRoomResponse.of(findMessage.getMember().getId(),
+                roomResList.add(ChatRoomResponse.of(chatRoom.getId(), findMessage.getMember().getId(),
                         findMessage.getContent(), findMessage.getSendTime()));
             }
         });
@@ -56,15 +53,14 @@ public class ChatController {
     }
 
     /**
-     * 문의 응답 확인하기 -> 세부 정보 클릭
-     * = 즉, 1:1 채팅방 내용
-     *
-     * 요청 데이터) roomId
-     * 응답 데이터) List<ChatRoomResponse>
-     * 내부 -> memberId, content, date
-     *
+     * 문의 응답 확인하기 (세부 정보)
+     * @param roomId
+     * @param memberId
+     * @return List<ChatRoomResponse>
      */
     @GetMapping("/inquiry/room/{roomId}")
+    @ApiOperation(value = "문의 응답 확인하기 (세부 정보)",
+            notes = "정상 동작 시 각 채팅방의 세부 정보 전달, 요소: roomId, memberId, content")
     public APIResponse getInquiryRoom(@PathVariable(value = "roomId") Long roomId,
                                       @PK Long memberId) {
         List<ChatRoomResponse> roomResList = new ArrayList<>();
@@ -76,36 +72,31 @@ public class ChatController {
         return APIResponse.of(roomResList);
     }
 
-
     /**
-     * 옥상지기에게 문의하기
-     * - '전송하기' 버튼을 사용자가 눌렀을 때, 옥상과 사용자 사이 이미 채팅방이 존재하는지 여부 확인
-     * 1) 존재하지 않는다면 채팅방 생성 + 해당 채팅방에 메시지 보내기
-     * 2) 존재한다면 해당 채팅방에 메시지 보내기
-     *
-     * 없으면 채팅방 id 새로 생성, 있으면 기존에 존재하는 거 리턴해주기
-     *
-     * 생각해보니까 옥상지기 정보가 아니라, 옥상 id가 필요할 것 같네...? (옥상별로 생기는 거니까...)
-     * 요청 데이터) 옥상 id, 사용자 id
-     * 응답 데이터) roomId
+     * 문의하기
+     * @param rooftopId
+     * @param memberId
+     * @return roomId
      */
     @GetMapping("/inquiry/join/room/{rooftopId}")
+    @ApiOperation(value = "문의하기 버튼 클릭 시 동작",
+            notes = "정상 동작 시 roomId 리턴")
     public APIResponse joinChatRoom(@PathVariable(value = "rooftopId") Long rooftopId,
                                     @PK Long memberId) {
         return APIResponse.of(chatRoomService.joinChatRoom(rooftopId, memberId));
     }
 
     /**
-     * 실제로 채팅방에 메시지 보내기
-     * - 클라이언트가 소켓 연결 후, /queue/roomId를 구독하면 될 듯...?
-     *
-     *  client 소켓에서 sendMessage 함수로 메시지를 보내서 @MessageMapping으로 받는 느낌?
+     * 메시지 전송
+     * @param chatRequest
+     * @return null
      */
     @MessageMapping("/inquiry/room")
-    public APIResponse chat(@RequestBody ChatRequest chatRequest) {
-        messageService.sendMessage(chatRequest.getRoomId(), chatRequest.getMemberId(), chatRequest.getContent());
-        messagingTemplate.convertAndSend("/queue/" + chatRequest.getRoomId(), chatRequest.getContent());
-        return APIResponse.of("채팅 전송 성공!");
+    @ApiOperation(value = "메시지 전송", notes = "정상 동작 시 memberId, roomId, content, sendTime 리턴")
+    public APIResponse chat(ChatRequest chatRequest) {
+        LocalDateTime sendTime = messageService.sendMessage(chatRequest.getRoomId(), chatRequest.getMemberId(), chatRequest.getContent());
+        messagingTemplate.convertAndSend("/queue/" + chatRequest.getRoomId(),
+                ChatRequest.of(chatRequest.getMemberId(), chatRequest.getRoomId(), chatRequest.getContent(), sendTime));
+        return APIResponse.empty();
     }
-
 }
