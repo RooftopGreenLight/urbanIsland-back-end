821e5c094c70efd764bdd92d73ce7014a08b34c8
jiwon <ljwon77@daum.net>
2022-08-05 23:57:51 +0900

    Fix: ChatRoomResponse DTO 수정

--- src/main/java/rooftopgreenlight/urbanisland/api/controller/dto/ChatRoomResponse.java
+++ src/main/java/rooftopgreenlight/urbanisland/api/controller/dto/ChatRoomResponse.java
@@ -1,14 +1,40 @@
 package rooftopgreenlight.urbanisland.api.controller.dto;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor(staticName = "of")
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ChatRoomResponse {
+    private Long roomId;
     private Long memberId;
     private String content;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime sendTime;
+
+    protected ChatRoomResponse(Long memberId, String content, LocalDateTime sendTime) {
+        this.memberId = memberId;
+        this.content = content;
+        this.sendTime = sendTime;
+    }
+
+    protected ChatRoomResponse(Long roomId, Long memberId, String content, LocalDateTime sendTime) {
+        this.roomId = roomId;
+        this.memberId = memberId;
+        this.content = content;
+        this.sendTime = sendTime;
+    }
+
+    public static ChatRoomResponse of(Long memberId, String content, LocalDateTime sendTime) {
+        return new ChatRoomResponse(memberId, content, sendTime);
+    }
+
+    public static ChatRoomResponse of(Long roomId, Long memberId, String content, LocalDateTime sendTime) {
+        return new ChatRoomResponse(roomId, memberId, content, sendTime);
+    }
+
 }
