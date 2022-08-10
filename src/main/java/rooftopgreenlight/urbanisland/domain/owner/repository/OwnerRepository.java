7adf766151d32ad7ac060177ad4cd199f13be2f0
최영찬 <gks54410@gmail.com>
2022-08-10 23:30:16 +0900

    Feat: #59 - 옥상지기 요청 수락/거절 기능 추가

--- src/main/java/rooftopgreenlight/urbanisland/domain/owner/repository/OwnerRepository.java
+++ src/main/java/rooftopgreenlight/urbanisland/domain/owner/repository/OwnerRepository.java
@@ -1,11 +1,21 @@
 package rooftopgreenlight.urbanisland.domain.owner.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 import rooftopgreenlight.urbanisland.domain.owner.entity.Owner;
+
+import java.util.Optional;
 
 public interface OwnerRepository extends
         JpaRepository<Owner, Long>,
         QuerydslPredicateExecutor<Owner>,
         OwnerRepositoryCustom{
+
+    @Query("select o from Owner o left join fetch o.member om where o.member.id = :memberId")
+    Optional<Owner> findByMemberIdWithMember(@Param("memberId") Long memberId);
+
+    @Query("select o from Owner o left join fetch o.ownerImage oi where o.member.id = :memberId")
+    Optional<Owner> findByMemberIdWithImage(@Param("memberId") Long memberId);
 }
