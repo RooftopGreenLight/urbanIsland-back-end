318176a5aff67893517262447f1a9046ff783066
jiwon <ljwon77@daum.net>
2022-08-11 23:32:02 +0900

    Feat: #65 - 녹화 안 된 옥상 조회 기능 추가

---
+++ src/main/java/rooftopgreenlight/urbanisland/domain/rooftop/service/dto/RooftopImageDto.java
@@ -0,0 +1,34 @@
+package rooftopgreenlight.urbanisland.domain.rooftop.service.dto;
+
+import lombok.Data;
+import lombok.ToString;
+import rooftopgreenlight.urbanisland.domain.file.entity.RooftopImage;
+import rooftopgreenlight.urbanisland.domain.file.entity.constant.ImageType;
+
+@Data
+@ToString
+public class RooftopImageDto {
+    private String type;
+    private String uploadFilename;
+    private String storeFilename;
+    private String fileUrl;
+    private ImageType rooftopImageType;
+
+    private RooftopImageDto(String type, String uploadFilename, String storeFilename, String fileUrl, ImageType rooftopImageType) {
+        this.type = type;
+        this.uploadFilename = uploadFilename;
+        this.storeFilename = storeFilename;
+        this.fileUrl = fileUrl;
+        this.rooftopImageType = rooftopImageType;
+    }
+
+    public static RooftopImageDto of(RooftopImage rooftopImage) {
+        return new RooftopImageDto(
+                rooftopImage.getType(),
+                rooftopImage.getUploadFilename(),
+                rooftopImage.getStoreFilename(),
+                rooftopImage.getFileUrl(),
+                rooftopImage.getRooftopImageType()
+        );
+    }
+}
