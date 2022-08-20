07e96b7f102a86b81ab41476a65447c76ac13f0a
jiwon <ljwon77@daum.net>
2022-08-20 23:48:48 +0900

    Fix: PeopleCount Getter 추가, 중복 리뷰 체크 제거

--- src/main/java/rooftopgreenlight/urbanisland/domain/rooftop/entity/RooftopPeopleCount.java
+++ src/main/java/rooftopgreenlight/urbanisland/domain/rooftop/entity/RooftopPeopleCount.java
@@ -1,12 +1,14 @@
 package rooftopgreenlight.urbanisland.domain.rooftop.entity;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Embeddable;
 
+@Getter
 @Embeddable
 @Access(AccessType.FIELD)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
