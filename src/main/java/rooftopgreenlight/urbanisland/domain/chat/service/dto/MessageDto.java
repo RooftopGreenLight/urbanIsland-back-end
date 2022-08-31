23f2a616777dafa454ea7123f19f387e3c0d6236
jiwon <ljwon77@daum.net>
2022-08-31 16:36:45 +0900

    Fix: 채팅 기능 응답 형태 DTO로 수정

--- src/main/java/rooftopgreenlight/urbanisland/domain/chat/service/dto/MessageDto.java
+++ src/main/java/rooftopgreenlight/urbanisland/domain/chat/service/dto/MessageDto.java
@@ -1,2 +1,20 @@
-package rooftopgreenlight.urbanisland.domain.chat.service.dto;public class MessageDto {
+package rooftopgreenlight.urbanisland.domain.chat.service.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class MessageDto {
+    private Long id;
+    private String content;
+    private LocalDateTime sendTime;
+    private Long memberId;
+
+    public static MessageDto of(Long id, String content, LocalDateTime sendTime, Long memberId) {
+        return new MessageDto(id, content, sendTime, memberId);
+    }
 }
