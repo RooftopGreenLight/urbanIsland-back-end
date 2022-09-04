4a9dc055e53b6fb45f56e88d78fd04d5bc089195
최영찬 <gks54410@gmail.com>
2022-09-04 17:21:31 +0900

    Feat: 자정에 만료된 Reservation 상태 변경 Job 추가

--- src/main/java/rooftopgreenlight/urbanisland/batch/job/ChangeReservationStatusJobConfig.java
+++ src/main/java/rooftopgreenlight/urbanisland/batch/job/ChangeReservationStatusJobConfig.java
@@ -2,21 +2,40 @@ package rooftopgreenlight.urbanisland.batch.job;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionException;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.JobExecutionNotFailedException;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+import rooftopgreenlight.urbanisland.domain.reservation.entity.PaymentStatus;
 import rooftopgreenlight.urbanisland.domain.reservation.entity.Reservation;
+import rooftopgreenlight.urbanisland.domain.reservation.entity.ReservationStatus;
+import rooftopgreenlight.urbanisland.domain.reservation.repository.ReservationRepository;
+
+import javax.persistence.EntityManagerFactory;
+import java.time.LocalDateTime;
+import java.util.Collections;
 
 @Configuration
 @RequiredArgsConstructor
-public class ChangeReservationStatusJob {
+public class ChangeReservationStatusJobConfig {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final ReservationRepository reservationRepository;
 
     @Bean
     public Job changeReservationStatusJob(final Step changeReservationStatusStep) {
@@ -28,12 +47,54 @@ public class ChangeReservationStatusJob {
 
     @Bean
     @JobScope
-    public Step changeReservationStatusStep() {
+    public Step changeReservationStatusStep(
+            final RepositoryItemReader<Reservation> reservationRepositoryItemReader,
+            final ItemWriter<Reservation> reservationItemWriter,
+            final ItemProcessor<Reservation, Reservation> reservationItemProcessor
+    ) {
         return stepBuilderFactory.get("changeReservationStatusStep")
-                .<Reservation, Void>chunk(100)
-                .reader()
-                .processor()
-                .writer()
+                .<Reservation, Reservation>chunk(100)
+                .reader(reservationRepositoryItemReader)
+                .processor(reservationItemProcessor)
+                .writer(reservationItemWriter)
                 .build();
+    }
+
+    @Bean
+    @StepScope
+    public RepositoryItemReader<Reservation> reservationRepositoryItemReader(
+            @Value("#{jobParameters[currentDateTime]}") String currentDateTime
+    ) {
+        return new RepositoryItemReaderBuilder<Reservation>()
+                .name("reservationRepositoryItemReader")
+                .repository(reservationRepository)
+                .methodName("findReservationsByEndDateBeforeAndReservationStatusAndPaymentStatus")
+                .pageSize(100)
+                .sorts(Collections.singletonMap("id", Sort.Direction.ASC))
+                .arguments(LocalDateTime.parse(currentDateTime).toLocalDate(), ReservationStatus.WAITING, PaymentStatus.PAYMENT_COMPLETED)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public ItemProcessor<Reservation, Reservation> reservationItemProcessor() {
+        return item -> {
+            item.changeReservationStatus(ReservationStatus.COMPLETED);
+            return item;
+        };
+    }
+
+    @Bean
+    @StepScope
+    public JpaItemWriter<Reservation> reservationItemWriter(
+            final EntityManagerFactory entityManagerFactory
+            ) throws Exception {
+        JpaItemWriter<Reservation> itemWriter = new JpaItemWriterBuilder<Reservation>()
+                .entityManagerFactory(entityManagerFactory)
+                .usePersist(true)
+                .build();
+
+        itemWriter.afterPropertiesSet();
+        return itemWriter;
     }
 }
--- src/main/java/rooftopgreenlight/urbanisland/batch/scheduler/ChangeReservationStatusScheduler.java
+++ src/main/java/rooftopgreenlight/urbanisland/batch/scheduler/ChangeReservationStatusScheduler.java
@@ -1,2 +1,40 @@
-package rooftopgreenlight.urbanisland.batch.scheduler;public class ChangeReservationStatusScheduler {
+package rooftopgreenlight.urbanisland.batch.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+
+@Configuration
+@RequiredArgsConstructor
+public class ChangeReservationStatusScheduler {
+    private final Job changeReservationStatusJob;
+    private final JobLauncher jobLauncher;
+
+    @Scheduled(cron = "0 0 0 * * *") // 자정 기준
+    public void executeChangeReservationStatusJob() {
+        try {
+            jobLauncher.run(
+                    changeReservationStatusJob,
+                    new JobParametersBuilder().addString("currentDateTime", LocalDateTime.now().toString())
+                            .toJobParameters()
+            );
+        } catch (JobExecutionAlreadyRunningException e) {
+            throw new RuntimeException(e);
+        } catch (JobRestartException e) {
+            throw new RuntimeException(e);
+        } catch (JobInstanceAlreadyCompleteException e) {
+            throw new RuntimeException(e);
+        } catch (JobParametersInvalidException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
