e341e2abebd17c9177077684e6fba361a87ed50b
최영찬 <gks54410@gmail.com>
2022-09-04 17:20:19 +0900

    Fix: NPE 수정

--- src/main/java/rooftopgreenlight/urbanisland/api/common/CustomAuditorAware.java
+++ src/main/java/rooftopgreenlight/urbanisland/api/common/CustomAuditorAware.java
@@ -1,6 +1,7 @@
 package rooftopgreenlight.urbanisland.api.common;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +12,10 @@ public class CustomAuditorAware implements AuditorAware<String> {
 
     @Override
     public Optional<String> getCurrentAuditor() {
-        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) return Optional.ofNullable("MASTER");
 
+        String name = authentication.getName();
         if(name.equals("anonymousUser")) return Optional.ofNullable("MASTER");
         else {
             return Optional.ofNullable(name);
