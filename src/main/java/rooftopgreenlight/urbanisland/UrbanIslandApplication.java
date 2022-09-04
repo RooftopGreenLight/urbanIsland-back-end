87741fc6a6caa4dffec598403a9542eee9256b8a
최영찬 <gks54410@gmail.com>
2022-09-04 17:20:53 +0900

    Feat: Batch, Scheduling 활성화

--- src/main/java/rooftopgreenlight/urbanisland/UrbanIslandApplication.java
+++ src/main/java/rooftopgreenlight/urbanisland/UrbanIslandApplication.java
@@ -6,16 +6,20 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
+@EnableScheduling
 @EnableJpaAuditing
+@EnableBatchProcessing
 @SpringBootApplication
 @ConfigurationPropertiesScan(basePackages = {
 		"rooftopgreenlight.urbanisland.domain.common.properties",
